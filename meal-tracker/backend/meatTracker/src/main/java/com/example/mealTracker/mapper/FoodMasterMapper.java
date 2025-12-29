1dd44f49cb5d980c7ba82ee34097fb7cbdf5aa70
Kojun123 <kwonojou@gmail.com>
2025-12-29 09:45:45 +0900

    커밋 테스트

--- meal-tracker/backend/meatTracker/src/main/java/com/example/mealTracker/mapper/FoodMasterMapper.java
+++ meal-tracker/backend/meatTracker/src/main/java/com/example/mealTracker/mapper/FoodMasterMapper.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Mapper
 public interface FoodMasterMapper {
+        //commit test
         FoodMaster findByName(String name);
         List<FoodMaster> findSimilarByName(@Param("name") String name);
 
